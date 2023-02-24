a75d6b7078be2436eef5b2e68e2e1cc05b811337
Anastasiia Hryhorzhevska <anastasiia_hry@psych.mpg.de>
2023-02-24 19:31:03 +0100

    remove unnecessary comments

--- code/integrative/phewas/03_data_harmonisation.R
+++ code/integrative/phewas/03_data_harmonisation.R
@@ -1,9 +1,6 @@
-# ------------------------------------
-# Data Harmonisation------------------
-# ------------------------------------
+# MR-PheWAS Analysis: Data Harmonisation
 
-
-# 1 Preparation-----------------------
+# 1 Preparation
 
 ## Load packages
 library("tidyverse")
@@ -14,9 +11,8 @@ library("ieugwasr")
 library("MRInstruments")
 
 ## Load data
-## 
-## dir.pre     <-"/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-array/"
-dir.pre       <- "~/bio/code/mpip/dex-stim-human-array/"
+
+dir.pre     <-"/binder/mgp/workspace/2020_DexStim_Array_Human/dex-stim-human-array/"
 phewas.dir    <- paste0(dir.pre, "data/integrative/phewas/8_cell_type_groups/")
 exposure.data <- readRDS(paste0(phewas.dir, "exposure_data_clumped.rds"))
 ao            <- readRDS(paste0(dir.pre, "/data/public_data/PheWAS/IEU_List_Selected_Outcomes.rds"))
@@ -39,13 +35,7 @@ saveRDS(outcome.data,
 
 # outcome.data <- readRDS(file = paste0(phewas.dir, "outcome_data_clumped.rds"))
 
-# 3 Harmonisation---------------------
-
-# exposure.data.no.base <- exposure.data[exposure.data$exposure %like% "no_base",]
-# outcome.data.no.base  <- outcome.data[outcome.data$SNP %in% exposure.data.no.base$SNP,]
-
-# exposure.data <- exposure.data.no.base # [1:8,]
-# outcome.data  <- outcome.data.no.base # [outcome.data.no.base$SNP %in% exposure.data$SNP,]
+# 3 Harmonisation
 
 data <- harmonise_data(
       exposure_dat = exposure.data,
@@ -53,7 +43,7 @@ data <- harmonise_data(
       action = 2
 )
 
-# 4 Bug fixing------------------------
+# 4 Bug fixing
 
 ## Some duplicate rows are present in the data. These are removed:
 harmonised.data <- unique(data)
@@ -70,7 +60,7 @@ harmonised.data <- harmonised.data %>%
   select(-n_snp, n_snp_1)
 
 
-# 5 Saving----------------------------
+# 5 Saving
 
 saveRDS(harmonised.data, 
         file = paste0(phewas.dir, "harmonised_data_clumped.rds"))
